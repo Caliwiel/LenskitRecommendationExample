70453c66f2931f47897da2a63aac9c9512e43781
Caliwiel <ebouzekri@gmail.com>
2016-04-30 15:42:42 +0200

    #7 Recommandations toujours identiques pour un même jeu de données

--- src/test/java/org/grouplens/lenskit/hello/ExampleTest.java
+++ src/test/java/org/grouplens/lenskit/hello/ExampleTest.java
@@ -1,14 +1,35 @@
 package org.grouplens.lenskit.hello;
 
+import org.lenskit.api.ResultList;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by anais on 30/04/16.
  */
 public class ExampleTest {
-    @org.junit.Test
-    public void run() throws Exception {
 
+    private static final int NB_RECS = 51;
+
+    @org.junit.Test
+    public void recommendationsIdentiques() throws Exception {
+        //Given
+        String[] args = new String[2];
+        args[0] = "user";
+        args[1] = "1";
+        Example example = new Example(args);
+        //When
+        ResultList[] recs = new ResultList[NB_RECS];
+        for (int i = 0; i<NB_RECS;i++) {
+            example.run();
+            recs[i] = example.getRecommandations();
+        }
+        //Then
+        for (int i=0;i<NB_RECS-1;i++) {
+            assertTrue(recs[i].equals(recs[i+1]));
+        }
     }
 
 }
